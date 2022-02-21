2b1a82b047bb8e1b580332eee816fc6da442baa8
Junnel Crucillo <jcrucillo2@nait.ca>
2022-02-21 22:17:28 +0000

    Isotope project done

--- js/main.js
+++ js/main.js
@@ -3,4 +3,19 @@ $(".container").imagesLoaded(function () {
         itemSelector: ".grid-item",
         layoutMode: "masonry",
     });
+
+    $(".item-menu").on("click", "li", function () {
+        let filterValue = $(this).attr("data-filter");
+
+        $grid.isotope({ filter: filterValue });
+    });
+});
+
+$(".selectGroup").each(function (i, selectGroup) {
+    let $selectGroup = $(selectGroup);
+
+    $selectGroup.on("click", "li", function () {
+        $selectGroup.find(".is-checked").removeClass("is-checked");
+        $(this).addClass("is-checked");
+    });
 });
